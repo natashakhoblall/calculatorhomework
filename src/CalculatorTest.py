b3ce70e6246f3707cb78cf602a4e9234bc04f068
Natasha Khoblall <nnk7@njit.edu>
2019-10-19 16:22:50 -0400

    Feature: Added test development files

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -3,14 +3,26 @@ from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
-    def test_add_method_calculator(self):
-        calculator = Calculator
-        self.assertEqual(calculator.add(2,2),4)
+    def test_addition(self):
+        self.assertEqual(self.calculator.add(1, 1), 2)
+        self.assertEqual(self.calculator.result, 2)
+
+
+    def test_subtraction(self):
+        self.assertEqual(self.calculator.subtract(1, 1), 0)
+        self.assertEqual(self.calculator.result, 0)
+
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
 
 if __name__ == '__main__':
     unittest.main()
