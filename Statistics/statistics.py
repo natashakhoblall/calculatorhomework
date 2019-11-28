c2a714ae89da83735d9cc57acfb8549caa8d448a
natashakhoblall <55815766+natashakhoblall@users.noreply.github.com>
2019-11-28 09:50:17 -0500

    Update statistics.py

--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -1,6 +1,19 @@
 from Calculator.Calculator import Calculator
-from Statistics.Mean import mean
-from Statistics.SampleMean import sample_mean
+from Statistics.Median import median
+from Statistics.Mode import mode
+from Statistics.SampleMean import samplemean
+from Statistics.PopulationMean import populationmean
+from Statistics.StandardDeviation import stddev
+from Statistics.Variance import variance
+from Statistics.PValue import pvalue
+from Statistics.Proportion import proportion
+from Statistics.ZScore import zscore
+from Statistics.CorrelationCoefficient import correlation
+from Statistics.ConfidenceIntervalBottom import confidence_interval_bottom
+from Statistics.SampleStandardDeviation import samplestddev
+from Statistics.SampleVariance import samplevariance
+from Statistics.VarianceOfPopulationProportion import variance_of_population_proportion
+from Statistics.ConfidenceIntervalTop import confidence_interval_top
 
 from CsvReader.CsvReader import CsvReader
 
@@ -18,3 +31,61 @@ class Statistics(Calculator):
     def sample_mean(self, sample_size):
         self.result = sample_mean(self.data, sample_size)
         return self.result
+    
+     def population_mean(self, data):
+        self.result = populationmean(data)
+        return self.result
+
+    def median(self, data):
+        self.result = median(data)
+        return self.result
+
+    def mode(self, data):
+        self.result = mode(data)
+        return self.result
+
+    def stddev(self, data):
+        self.result = stddev(data)
+        return self.result
+
+    def variance(self, data):
+        self.result = variance(data)
+        return self.result
+
+    def proportion(self, data):
+        self.result = proportion(data)
+        return self.result
+
+    def zscore(self, data):
+        self.result = zscore(data)
+        return self.result
+
+    def correlation_coefficient(self, data, data1):
+        self.result = correlation(data, data1)
+        return self.result
+
+    def population_proportion_variance(self, data):
+        self.result = variance_of_population_proportion(data)
+        return self.result
+
+    def confidence_interval_top(self, data):
+        self.result = confidence_interval_top(data)
+        return self.result
+
+    def confidence_interval_bottom(self, data):
+        self.result = confidence_interval_bottom(data)
+        return self.result
+    
+     def p_value(self, data):
+        self.result = pvalue(data)
+        return self.result
+
+    def sample_stddev(self, data):
+        self.result = samplestddev(data)
+        return self.result
+
+    def sample_variance(self, data):
+        self.result = samplevariance(data)
+        return self.result
+
+    pass
