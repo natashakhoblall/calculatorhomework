7a0019f5d456aafc4042e0f1283375ef82baeadc
natashakhoblall <55815766+natashakhoblall@users.noreply.github.com>
2019-12-01 13:18:52 -0500

    Feature: added standardized score function

---
+++ Statistics/StandardizedScore.py
@@ -0,0 +1,13 @@
+def score(a, b, c):
+    try:
+        a = int(a)
+        b = int(b)
+        c = int(c)
+    ## a is value
+    ## b is mean
+    ## c is standard deviation
+        d = (a - b) / c
+        return d
+    except ZeroDivisionError:
+        print("Error: Wrong number, not valid!!")
+    except ValueError:
