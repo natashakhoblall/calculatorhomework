cef9780f827a03180c19091625939d0b168543de
natashakhoblall <55815766+natashakhoblall@users.noreply.github.com>
2019-12-01 13:16:22 -0500

    Feature: added population standard deviation function

---
+++ Statistics/PopulationStandardDeviation.py
@@ -0,0 +1,11 @@
+def population_standard_deviation(a, b):
+    try:
+       a = int(a)
+       b = int(b)
+       c = (a ** 2) / b
+       return c
+    except ZeroDivisionError:
+        print("Error: Wrong number, not valid!!")
+    except ValueError:
+        print ("Error: Only Numeric Values are valid!!")
+© 2019 GitHub, Inc.
